ce250d5391b784136583430f178b2ee488274727
Hector <104199317+hector-jurado@users.noreply.github.com>
2026-03-28 18:25:06 +0100

    Remove commented-out task management routes
    Removed commented-out task management routes.

--- server.js
+++ server.js
@@ -16,39 +16,3 @@ app.get("/", (req,res)=>{
 app.listen(PORT,()=>{
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 })
-
-// //crear tarea
-// app.post("/tasks",(req,res)=>{
-//     const newTask ={
-//         id: id++,
-//         title: req.body.title
-//     };
-//     task.push(newTask);
-//     res.json(newTask)
-// });
-// //obtener tareas
-// app.get("/tasks",(req,res)=>{
-//     res.json(task);
-// })
-// //eliminar tareas
-// app.delete("/tasks/:id",(req,res)=>{
-//     const taskId = parseInt(req.params.id);
-
-//     task = task.filter(t => t.id !== taskId);
-
-//     res.json({message: "Tarea eliminada"})
-// });
-// //editar tareas
-// app.put("/tasks/:id",(req, res) => {
-//     const taskId = parseInt(req.params.id);
-
-//     const foundTask = task.find(t => t.id === taskId);
-
-//     if (!foundTask){
-//         return res.status(404).json({message: "Tarea no encontrada"});
-
-//     }
-//     foundTask.title = req.body.title;
-    
-//     res.json(foundTask);
-// });
